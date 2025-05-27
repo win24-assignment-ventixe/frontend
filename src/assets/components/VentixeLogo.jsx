4e72c7ed3700d4477206eb1ef93252bc3f98dc6f
Morgan Johansson <morgan.johansson@me.com>
2025-05-27 16:00:56 +0200

    BrokenLogotype

--- src/assets/components/VentixeLogo.jsx
+++ src/assets/components/VentixeLogo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const VentixeLogo = () => {
   return (
     <Link to='/' className='logotype' >
-    <img src='src/assets/ventixe_logo.svg' alt='Ventixe logotype' />
+    <img src='/src/assets/ventixelogo.svg' alt='Ventixe logotype' />
     <span>Ventixe</span>
     </Link>
   )
